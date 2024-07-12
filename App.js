4fd606f8efdd8fc63a008319abc29a9b5b69713c
Sumit Umrao <129044589+SumitUmrao@users.noreply.github.com>
2024-07-12 09:53:23 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Form from './components/Form';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
